03c78770f963077963df5c6c86bae80fab0a5c57
Your Name <you@example.com>
2024-11-04 18:46:26 -0800

    Unit Test Libraries added to `my-project.nix`

--- my-project.nix
+++ my-project.nix
@@ -1,11 +1,15 @@
-{ mkDerivation, base, HDBC, HDBC-sqlite3, lib }:
+{ mkDerivation, base, HDBC, HDBC-sqlite3, hspec, HUnit, lib
+, QuickCheck
+}:
 mkDerivation {
   pname = "SqlInjectionTest";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base HDBC HDBC-sqlite3 ];
+  executableHaskellDepends = [
+    base HDBC HDBC-sqlite3 hspec HUnit QuickCheck
+  ];
   license = lib.licenses.bsd3;
   mainProgram = "SqlInjectionTest";
 }
